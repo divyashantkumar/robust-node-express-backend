78734fbb6e5545f90bf52caaf0b830e1cb0224ca
divyashantkumar <divyashantkumar000@gmail.com>
2025-01-14 19:36:59 +0530

    updated: globalErrorHandler.js

--- src/middlewares/globalErrorHandler.js
+++ src/middlewares/globalErrorHandler.js
@@ -4,17 +4,21 @@ import { Error as MongooseError } from "mongoose";
 // Handle specific MongoDB errors
 export const handleMongoError = (err) => {
     if (err.name === 'CastError') {
-        return new CustomError(`Invalid ${err.path}: ${err.value}`, 400);
+        const e = new CustomError(`Invalid ${err.path}: ${err.value}`, 400, err?.errors);
+        return e.serializeErrors();
     }
     if (err.code === 11000) {
         const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
-        return new CustomError(`Duplicate field value: ${value}. Please use another value!`, 400);
+        const e = new CustomError(`Duplicate field value: ${value}. Please use another value!`, 400, err?.errors);
+        return e.serializeErrors();
     }
     if (err.name === 'ValidationError') {
         const errors = Object.values(err.errors).map(el => el.message);
-        return new CustomError(`Invalid input data. ${errors.join('. ')}`, 400);
+        const e = new CustomError(`Invalid input data - ${errors.join('. ')}`, 400, err?.errors);
+        return e.serializeErrors();
     }
-    return err;
+    const e = new CustomError(err?.message || 'Something went wrong', 400, err?.errors);
+    return e.serializeErrors();
 };
 
 export const globalErrorHandler = (err, req, res, next) => {
@@ -22,20 +26,18 @@ export const globalErrorHandler = (err, req, res, next) => {
     const errorMessage = err.message || 'Something went wrong!';
 
     if (err instanceof CustomError) {
+        const e = new CustomError(errorMessage, statusCode, err?.errors);
         return res
-            .status(statusCode)
-            .json(
-                new CustomError(errorMessage, statusCode, err.errors)
-            );
+            .status(e.statusCode)
+            .json({ ...e.serializeErrors() });
     } else if (err instanceof MongooseError) {
+        const serializeErrors = handleMongoError(err);
         return res
-            .status(statusCode)
-            .json(
-                handleMongoError(err)
-            );
+            .status(serializeErrors?.statusCode)
+            .json({ ...serializeErrors });
     }
 
-    return res.status(500).json({ status: 'error', message: 'Something went wrong! Internal Server Error' });
+    return res.status(500).json({ status: 'error', message: 'Something went wrong! Internal Server Error', errors: err?.errors });
 };
 
 
